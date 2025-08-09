a78516cf1e9509704ad783386b5b6309cc0c8ca6
taha <t.sabiri>
2025-08-09 10:34:02 +0330

    responsive

--- src/app/(public)/about-us/page.tsx
+++ src/app/(public)/about-us/page.tsx
@@ -15,14 +15,14 @@ export default function AboutUs() {
                 <div className=" h-full  flex w-full">
                 </div>
             </div>
-            <div className="text-2xl h-[47.5svh]  border-b border-koosha-pr flex justify-center items-center px-[13svw] ">
-                <div className="border-l border-koosha-pr h-full  flex w-full items-center ">
-                    <p className="text-sm w-[90%]">در کارگزاری ایده آل، با سرویس همراهی اختصاصی شما یک اکانت منیجر اختصاصی خواهید داشت که در هر لحظه در کنار شماست. این متخصص حرفه‌ای با دانش بازار و تجربه بالا، شما را در هر مرحله از تصمیم‌گیری‌های مالی همراهی می‌کند تا مطمئن باشید که تصمیمات شما همواره درست و آگاهانه است. هدف ما ایجاد تجربه‌ای مطمئن و اختصاصی برای هر مشتری است.
+            <div className="text-2xl h-[47.5svh] py-4 md:py-0 border-b border-koosha-pr flex  md:flex-row justify-center items-center px-[13svw] flex-col-reverse j  ">
+                <div className="border-none md:border-l border-koosha-pr h-full  flex w-full items-center justify-center ">
+                    <p className="text-sm w-[90%] text-center md:text-right  ">در کارگزاری ایده آل، با سرویس همراهی اختصاصی شما یک اکانت منیجر اختصاصی خواهید داشت که در هر لحظه در کنار شماست. این متخصص حرفه‌ای با دانش بازار و تجربه بالا، شما را در هر مرحله از تصمیم‌گیری‌های مالی همراهی می‌کند تا مطمئن باشید که تصمیمات شما همواره درست و آگاهانه است. هدف ما ایجاد تجربه‌ای مطمئن و اختصاصی برای هر مشتری است.
 
                         در کارگزاری ایده آل، با سرویس همراهی اختصاصی شما یک اکانت منیجر اختصاصی خواهید داشت که در هر لحظه در کنار شماست. این متخصص حرفه‌ای با دانش بازار و تجربه بالا، شما را در هر مرحله از تصمیم‌گیری‌های مالی همراهی می‌کند تا مطمئن باشید که تصمیمات شما همواره درست و آگاهانه است. هدف ما ایجاد تجربه‌ای مطمئن و اختصاصی برای هر مشتری است.</p>
                 </div>
-                <div className=" h-full  flex w-full">
-                    <Image src={about} alt="" className="w-auto h-fit" />
+                <div className=" h-full  flex w-full relative  border-r  border-koosha-pr" >
+                    <Image src={about} alt="" fill className="w-auto h-fit object-cover" />
                 </div>
             </div>
             <div className="text-2xl h-[10svh]   border-b border-koosha-pr flex justify-center items-center px-[13svw] ">
@@ -33,7 +33,7 @@ export default function AboutUs() {
                 </div>
             </div>
 
-            <div className=" grid grid-cols-3 gap-20 px-[13svw] py-[5svh]  h-[80svh]">
+            <div className=" grid grid-cols-1 md:grid-cols-3 gap-20 px-[13svw] py-[5svh]  md:h-[80svh] h-[150svh]">
 
                 <div className="flex flex-col">
                     <div className=' w-full h-full  relative  '>
@@ -110,20 +110,20 @@ export default function AboutUs() {
                 </div>
             </div>
 
-            <div className="  py-[20svh] border-b border-koosha-pr flex flex-col justify-center items-center px-[13svw] gap-6 ">
+            <div className="  py-[10svh] md:py-[20svh] border-b border-koosha-pr flex flex-col justify-center items-center px-[13svw] gap-6 ">
                 <p className="text-4xl text-koosha-pr font-black">آکادمی سرمایه‌گذاری ایده‌آل</p>
-                <p className="w-[40svw] text-center">
+                <p className="w:[95svw] md:w-[40svw] text-center">
                     در کارگزاری ایده آل، همراهی در تمام خدمات در نظر گرفته شده است. ما بر این باور هستیم که با ارایه آموزش‌های تخصصی بازار سرمایه و بورس به صورت رایگان از سطح مبتدی با حرفه‌ای شما را برای موفقیت در این بازار همراهی کنیم. برنامه‌های آموزشی ایده‌آل در سه مدل مختلف طراحی شده است. تقویم آموزش برخط روزانه و هفتگی، مقالات و تحلیل‌های تخصصی بازار سرمایه و ویدیو‌های آموزشی ضبط شده، از جمله همراهی ما برای آموزش شما است.
                 </p>
             </div>
 
-            <div className="  py-[20svh] bg-koosha-pr flex flex-col justify-center items-center px-[13svw] gap-6 ">
+            <div className=" text-center   py-[10svh] md:py-[20svh] bg-koosha-pr flex flex-col justify-center items-center px-[13svw] gap-6 ">
                 <p className="text-4xl text-white font-black">برای شروع سرمایه‌گذاری نیاز به مشاورین ایده‌آل دارید؟</p>
                 <div className="flex text-2xl text-white gap-2 items-center justify-center  ">
                     <p className="">چت آنلاین</p>
-                    <Image src={Phone} alt="" className="w-[2svw]" />
+                    <Image src={Phone} alt="" className="md:w-[2svw]" />
                     <p>۰۲۱۹۲۰۰۵۲۲۰</p>
-                    <Image src={Phone} alt="" className="w-[2svw]" />
+                    <Image src={Phone} alt="" className="md:w-[2svw]" />
 
                 </div>
             </div>
--- src/app/(public)/call-with-us/page.tsx
+++ src/app/(public)/call-with-us/page.tsx
@@ -19,26 +19,26 @@ export default function CallWithUs() {
             </div>
 
             <div className="  border-t border-b border-koosha-pr flex justify-center items-center px-[13svw]  ">
-                <div className=" h-full  flex w-full items-center flex-col gap-5  ">
-                    <div className="flex gap-2 items-center  justify-between w-[30svw]">
+                <div className=" h-full  flex w-full items-center flex-col gap-5 py-6  ">
+                    <div className="flex gap-2 items-center   justify-between w-[90svw] md:w-[30svw]">
                         <Image src={map} alt="" className="w-[2svw]" />
 
                         <p className="">خیابان ولیعصر، خیابان عباسپور (توانیر شمالی)، کوچه هومان، پلاک 2، طبقه اول</p>
 
                     </div>
-                    <div className="flex gap-2 items-center w-[30svw]">
+                    <div className="flex gap-2 items-center  w-[90svw] md:w-[30svw]">
                         <Image src={call} alt="" className="w-[2svw]" />
                         <div className="border-t border-koosha-pr w-full"></div>
                         <p className="">Hejazi@ideal.com</p>
 
                     </div>
-                    <div className="flex gap-2 items-center w-[30svw]">
+                    <div className="flex gap-2 items-center w-[90svw] md:w-[30svw]">
                         <Image src={call} alt="" className="w-[2svw]" />
                         <div className="border-t border-koosha-pr w-full "></div>
                         <p className=" w-fit text-nowrap">info@ideal-brokerage.ir</p>
 
                     </div>
-                    <div className="flex gap-2 items-center w-[30svw]">
+                    <div className="flex gap-2 items-center  w-[90svw] md:w-[30svw]">
                         <p className=" w-fit text-nowrap">ایده‌آل را در فضای مجازی دنبال کنید</p>
                         <div className="border-t border-koosha-pr w-full "></div>
 
@@ -48,7 +48,7 @@ export default function CallWithUs() {
 
                     </div>
                 </div>
-                <div className=" h-full flex w-full border-r border-koosha-pr">
+                <div className=" h-full  w-full border-r border-koosha-pr hidden md:flex" >
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25915.602742138326!2d51.36842749950012!3d35.715141500024!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f8e00491ff3dcd9%3A0xf0b3697c567024bc!2sTehran%2C%20Tehran%20Province%2C%20Iran!5e0!3m2!1sen!2sde!4v1754717414887!5m2!1sen!2sde" width="600" height="450" loading="lazy" ></iframe>
                 </div>
             </div>
@@ -59,7 +59,7 @@ export default function CallWithUs() {
                 <div className=" h-full  flex w-full">
                 </div>
             </div>
-            <div className="px-[20svw] py-[2svw] grid grid-cols-2 gap-4">
+            <div className="px-[20svw] py-[2svw] grid grid-cols-1 md:grid-cols-2 gap-4">
                 <div className="grid w-full items-center gap-3">
                     <Label htmlFor="name">نام</Label>
                     <Input type="name" id="name" placeholder="نام" />
@@ -68,22 +68,22 @@ export default function CallWithUs() {
                     <Label htmlFor="name">نام</Label>
                     <Input type="name" id="name" placeholder="نام" />
                 </div>
-                <div className="grid w-full items-center gap-3 col-span-2">
+                <div className="grid w-full items-center gap-3 md:col-span-2">
                     <Label htmlFor="name">آدرس ایمیل</Label>
                     <Input type="name" id="name" placeholder="آدرس ایمیل" />
                 </div>
-                <div className="grid w-full gap-3 col-span-2">
+                <div className="grid w-full gap-3 md:col-span-2">
                     <Label htmlFor="message">توضیحات</Label>
                     <Textarea placeholder="توضیحات" id="message" className="w-full" />
                 </div>
             </div>
             <div className="  py-[20svh] bg-koosha-pr flex flex-col justify-center items-center px-[13svw] gap-6 ">
-                <p className="text-4xl text-white font-black">برای شروع سرمایه‌گذاری نیاز به مشاورین ایده‌آل دارید؟</p>
+                <p className="text-[5svw] md:text-[2svw] text-center text-white font-black">برای شروع سرمایه‌گذاری نیاز به مشاورین ایده‌آل دارید؟</p>
                 <div className="flex text-2xl text-white gap-2 items-center justify-center  ">
                     <p className="">چت آنلاین</p>
-                    <Image src={Phone} alt="" className="w-[2svw]" />
+                    <Image src={Phone} alt="" className="md:w-[2svw]" />
                     <p>۰۲۱۹۲۰۰۵۲۲۰</p>
-                    <Image src={Phone} alt="" className="w-[2svw]" />
+                    <Image src={Phone} alt="" className="md:w-[2svw]" />
 
                 </div>
             </div>
